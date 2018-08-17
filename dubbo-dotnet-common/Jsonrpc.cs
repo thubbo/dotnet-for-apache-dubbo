ee82c429eb3f7a6e67e398551f11511730713541
kimmking <kimmking@163.com>
2018-08-18 01:47:40 +0800

    add jsonrpc without stub

---
+++ dubbo-dotnet-common/Jsonrpc.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Net;
+using System.Net.Http;
+
+namespace dubbo.dotnet.common
+{
+    [DataContract]
+    public class RequestMessage
+    {
+        [DataMember(Name="id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "jsonrpc")]
+        public string Version { get; set; }
+
+        [DataMember(Name = "method")]
+        public string Method { get; set; }
+
+        [DataMember(Name = "params")]
+        public object[] Parameters { get; set; }
+    }
+
+    [DataContract]
+    public class ResponseMessage<T>
+    {
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "jsonrpc")]
+        public string Version { get; set; }
+
+        [DataMember(Name = "result")]
+        public T result { get; set; }
+    }
+
+    public class Json
+    {
+        public static string serialize(object o)
+        {
+            DataContractJsonSerializer js = new DataContractJsonSerializer(o.GetType());
+            MemoryStream msObj = new MemoryStream();
+
+            js.WriteObject(msObj, o);
+            msObj.Position = 0;
+
+            StreamReader sr = new StreamReader(msObj, Encoding.UTF8);
+            string json = sr.ReadToEnd();
+            sr.Close();
+            msObj.Close();
+
+            return json;
+        }
+
+        public static T deserialize<T>(string json) where T : class
+        {
+            var memoryStream = new MemoryStream(Encoding.Unicode.GetBytes(json));
+            var deseralizer = new DataContractJsonSerializer(typeof(T));
+            var model = deseralizer.ReadObject(memoryStream);
+            memoryStream.Close();
+            return (T)model ;
+        }
+    }
+
+    public class HttpClient
+    {
+        public static string post(string url, string body)
+        {
+            HttpWebRequest request = HttpWebRequest.CreateHttp(url);
+            request.Method = "POST";
+            
+            using (var reqStream =  request.GetRequestStream())
+            {
+                var data = Encoding.UTF8.GetBytes(body);
+                reqStream.Write(data, 0, data.Length);
+            }
+
+            using (var resStream = request.GetResponse().GetResponseStream())
+            {
+                return new StreamReader(resStream, Encoding.UTF8).ReadToEnd();
+            }
+
+        }
+    }
+
+    public class JsonrpcClient
+    {
+        private static long _id = 1000;
+        public static string invokeJson(string url,string method, object[] parameters)
+        {
+            RequestMessage message = new RequestMessage()
+            {
+                Id = _id++.ToString(),
+                Version = "2.0",
+                Method = method,
+                Parameters = parameters
+            };
+
+            string req = Json.serialize(message);
+            return HttpClient.post(url, req);
+        }
+
+        public static T invoke<T>(string url, string method, object[] parameters) where T : class
+        {
+            string json = invokeJson(url, method, parameters);
+            return Json.deserialize<T>(json);
+        } 
+
+    }
+}
--- dubbo-dotnet-demo/Program.cs
+++ dubbo-dotnet-demo/Program.cs
@@ -2,16 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using dubbo.dotnet.common;
 
-namespace dubbo_dotnet_demo
+namespace dubbo.dotnet.demo
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Dubbo-Dotnet-Demo starting...");
+           
+
+             //curl -X POST -d {\"id\":\"-9072391164073734559\",\"jsonrpc\":\"2.0\",\"method\":\"getUser\",\"params\":[2]} 
+            string url = "http://192.168.99.1:10010/com.ikurento.user.UserProvider3";
+            string json = JsonrpcClient.invokeJson(url, "getUser", new object[] { 2 });
+
+            Console.WriteLine(json.ToString());
+
+            User user = Json.deserialize<ResponseMessage<User>>(json).result;
+            Console.WriteLine(user.ToString());
+
             Console.ReadKey();
+        }
+    }
+
+   
+
+    public class User
+    {
+        public string id { get; set; }
+
+        public string name { get; set; }
+
+        public int age { get; set; }
+
+        public long time { get; set; }
+
+        public string sex { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("id={0},name={1},age={2},time={3},sex={4}",id,name,age,time,sex);
         }
     }
 }
