eaf70624ce9b7ae2a5f6a84362182d3339fb7985
kimmking <kimmking@163.com>
2018-08-19 18:55:48 +0800

    add proxyService for jsonrpc

--- dubbo-dotnet-common/Jsonrpc.cs
+++ dubbo-dotnet-common/Jsonrpc.cs
@@ -5,8 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
+using System.Runtime.Remoting.Proxies;
+using System.Runtime.Remoting.Activation;
+using System.Runtime.Remoting.Messaging;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
+
+using Castle.Core;  
+using Castle.DynamicProxy;  
 
 namespace dubbo.dotnet.common
 {
@@ -57,13 +64,24 @@ namespace dubbo.dotnet.common
             return json;
         }
 
+        public static ResponseMessage<T> deserializeResponseMessage<T>(string json) where T : class
+        {
+            return deserialize(json, typeof(ResponseMessage<T>)) as ResponseMessage<T>;
+        }
+
+
         public static T deserialize<T>(string json) where T : class
         {
+            return deserialize(json,typeof(T)) as T;
+        }
+
+        public static object deserialize(string json, Type type)
+        {
             var memoryStream = new MemoryStream(Encoding.Unicode.GetBytes(json));
-            var deseralizer = new DataContractJsonSerializer(typeof(T));
+            var deseralizer = new DataContractJsonSerializer(type);
             var model = deseralizer.ReadObject(memoryStream);
             memoryStream.Close();
-            return (T)model ;
+            return model;
         }
     }
 
@@ -71,7 +89,7 @@ namespace dubbo.dotnet.common
     {
         public static string post(string url, string body)
         {
-            HttpWebRequest request = HttpWebRequest.CreateHttp(url);
+            HttpWebRequest request = WebRequest.CreateHttp(url);
             request.Method = "POST";
             
             using (var reqStream =  request.GetRequestStream())
@@ -108,8 +126,66 @@ namespace dubbo.dotnet.common
         public static T invoke<T>(string url, string method, object[] parameters) where T : class
         {
             string json = invokeJson(url, method, parameters);
-            return Json.deserialize<T>(json);
+            ResponseMessage<T> response = Json.deserializeResponseMessage<T>(json);
+            return response.result;
         } 
 
     }
+
+    public class JsonrpcInterceptor : IInterceptor
+    {
+        string baseUrl = "http://localhost/";
+
+        public JsonrpcInterceptor() { }
+        public JsonrpcInterceptor(string url)
+        {
+            this.baseUrl = url;
+            if (!this.baseUrl.EndsWith("/")) this.baseUrl = this.baseUrl + "/";
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            MethodInfo method = invocation.GetConcreteMethod();
+            Type type = method.DeclaringType;
+
+            if (method.DeclaringType.IsInterface)
+            {
+                string serviceUrl = this.baseUrl + type.FullName;
+                string methodName = method.Name;
+                object[] parameters = invocation.Arguments;
+
+                // invoke JSONRPC via http
+                string json = JsonrpcClient.invokeJson(serviceUrl, methodName, parameters);
+
+                Console.WriteLine(json.ToString());
+                Type retType = method.ReturnType;
+
+                // deserializeResponseMessage and make generic
+                MethodInfo dmethod = typeof(Json).GetMethod("deserializeResponseMessage");
+                MethodInfo generic = dmethod.MakeGenericMethod(retType);
+
+                // Json.deserializeResponseMessage(json)
+                object respose = generic.Invoke(null, new object[] { json });
+
+                // ResponseMessage.result
+                object ret = respose.GetType().GetProperty("result").GetValue(respose);
+
+                invocation.ReturnValue = ret;
+            }
+        }
+
+    }
+
+    public class JsonrpcProxy
+    {
+
+        static ProxyGenerator generator = new ProxyGenerator();
+
+        public static T proxyService<T>(string baseUrl) where T : class
+        {
+            JsonrpcInterceptor interceptor = new JsonrpcInterceptor(baseUrl);
+            return generator.CreateInterfaceProxyWithoutTarget<T>(interceptor);
+        }
+    }
+
 }
--- dubbo-dotnet-demo/Program.cs
+++ dubbo-dotnet-demo/Program.cs
@@ -6,31 +6,26 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
+using System.Runtime.Remoting.Proxies;
+using System.Runtime.Remoting.Activation;
+using System.Runtime.Remoting.Messaging;
+using System.Reflection;  
+
+using Castle.Core;  
+using Castle.DynamicProxy;  
+
 using dubbo.dotnet.common;
 
-namespace dubbo.dotnet.demo
+using com.ikurento.user;
+
+
+namespace com.ikurento.user
 {
-    class Program
+
+    public interface UserProvider3
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Dubbo-Dotnet-Demo starting...");
-           
-
-             //curl -X POST -d {\"id\":\"-9072391164073734559\",\"jsonrpc\":\"2.0\",\"method\":\"getUser\",\"params\":[2]} 
-            string url = "http://192.168.99.1:10010/com.ikurento.user.UserProvider3";
-            string json = JsonrpcClient.invokeJson(url, "getUser", new object[] { 2 });
-
-            Console.WriteLine(json.ToString());
-
-            User user = Json.deserialize<ResponseMessage<User>>(json).result;
-            Console.WriteLine(user.ToString());
-
-            Console.ReadKey();
-        }
+        User getUser(int id);
     }
-
-   
 
     public class User
     {
@@ -46,7 +41,47 @@ namespace dubbo.dotnet.demo
 
         public override string ToString()
         {
-            return String.Format("id={0},name={1},age={2},time={3},sex={4}",id,name,age,time,sex);
+            return String.Format("id={0},name={1},age={2},time={3},sex={4}", id, name, age, time, sex);
         }
     }
 }
+
+namespace dubbo.dotnet.demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dubbo-Dotnet-Demo starting...");
+
+            string baseUrl = "http://localhost:10010";
+   
+            UserProvider3 userProvider = JsonrpcProxy.proxyService<UserProvider3>(baseUrl);
+
+            User user3 = userProvider.getUser(2);
+            Console.WriteLine("final return: "+user3.ToString());
+
+            Console.ReadKey();
+        }
+
+        public static void TestJsonrpc()
+        {
+            //curl -X POST -d {\"id\":\"-9072391164073734559\",\"jsonrpc\":\"2.0\",\"method\":\"getUser\",\"params\":[2]} 
+            string url = "http://localhost:10010/com.ikurento.user.UserProvider3";
+            string json = JsonrpcClient.invokeJson(url, "getUser", new object[] { 2 });
+
+            Console.WriteLine(json.ToString());
+            // {"jsonrpc":"2.0","id":"1000","result":
+            //      {"id":"2","name":"userCode get","age":48,"time":1534659569624,"sex":"MAN"}}
+
+            User user = Json.deserializeResponseMessage<User>(json).result;
+            Console.WriteLine(user.ToString());
+        }
+    }
+
+   
+   
+}
+
+
+
